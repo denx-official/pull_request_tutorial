6eea62c9a36be077258e15befec2a5aa64d86765
ippee <ippee.music@gmail.com>
2021-08-07 19:09:49 +0900

    全てのjsonファイルをチェックするテストの追加

--- src/story.rs
+++ src/story.rs
@@ -1,3 +1,5 @@
+use log;
+
 use serde::{Deserialize, Serialize};
 
 use std::fs::File;
@@ -15,8 +17,32 @@ pub fn json_to_stories(files: Vec<File>) -> Vec<Story> {
     files
         .iter()
         .map(|file|{
+            log::error!("file: {:?}", file);
             let reader = BufReader::new(file);
             serde_json::from_reader(reader).expect("deserialization failed")
         })
         .collect()
+}
+
+#[cfg(test)]
+mod test {
+    use env_logger;
+    use crate::story::json_to_stories;
+    use std::fs::{File, read_dir};
+
+    #[test]
+    fn check_all_json_files() {
+        let _ = env_logger::builder().is_test(true).try_init();
+
+        let files = get_all_json_files();
+        json_to_stories(files);
+    }
+
+    fn get_all_json_files() -> Vec<File> {
+        read_dir("./json")
+            .expect("directory not found")
+            .map(|path|path.unwrap().path().display().to_string())
+            .map(|name|File::open(name).expect("file not found"))
+            .collect()
+    }
 }
